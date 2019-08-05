e51b5a654f2f91630ccdebaabdc5539992bb7d28
itamar.pedreira <itasouza@yahoo.com.br>
2019-08-05 15:32:48 -0300

    exemplo usando o ckeditor

--- ExemploTrabalho/js/ckeditor/ckeditor-config.js
+++ ExemploTrabalho/js/ckeditor/ckeditor-config.js
@@ -22,4 +22,19 @@ $(document).ready(function () {
             // console.error( error );
      } );
 
+
+     DecoupledEditor.create( document.querySelector('.document-editor__editable'),{
+        language:"pt-br",
+     })
+     .then( editor => {
+         const toolbarContainer = document.querySelector('.document-editor__toolbar');
+  
+         toolbarContainer.appendChild(editor.ui.view.toolbar.element);
+  
+         window.editor = editor;
+     } )
+     .catch( err => {
+         console.error( err );
+     } );
+
 });
